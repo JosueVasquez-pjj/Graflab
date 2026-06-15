ff4053bb20271cbc6218889e9cc6b10eb374634f
JosueVasquez-pjj <joslenovasques12@gmail.com>
2026-06-14 20:56:30 -0600

    Update artistas.js

--- js/artistas.js
+++ js/artistas.js
@@ -1,9 +1,10 @@
 fetch("data/artistas.json") // obtiene los datos del json
 .then(res => res.json()) // convierte la respuesta del fetch en un json
 .then(artistas => { //artistas es el nombre del json y toma toda la lista de elementos que contenga
+    
+    const contenedor = document.getElementById("contenedor-artistas");
+    
     artistas.forEach(artista => { //para cada elemento del json realizar lo siguiente
-        const contenedor = document.getElementById("contenedor-artistas");
-        
         contenedor.innerHTML += `
         <a class="card"
             href="detalles.html?tipo=artista&id=${artista.id}">
