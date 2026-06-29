815f6414ceb2b51eb8c616d953c7e0238248ec45
JosueVasquez-pjj <joslenovasques12@gmail.com>
2026-06-28 22:11:30 -0600

    Delete js directory

--- js/imageViewerLoader.js
+++
@@ -1,9 +0,0 @@
-fetch("components/imageViewer.html")
-.then(res=>res.text())
-.then(data=>{
-
-    document.getElementById("image-viewer-container").innerHTML=data;
-    
-    iniciarImageViewer();
-
-});
